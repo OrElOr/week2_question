28e64c050e3e4b50c6906fa2c074d5724a8db4af
OrElOr <89505175+OrElOr@users.noreply.github.com>
2025-03-17 17:48:07 +0900

    Update test.py

--- test.py
+++ test.py
@@ -57,7 +57,7 @@ class TestPriorityQueue(unittest.TestCase):
     pq.push("Task 3", 2)
 
     self.assertEqual(pq.pop(), "Task 2")
-    self.assertEqual(pq.pop(), "Task 3")
+    self.assertEqual(pq.pop(), "Task 1")
     self.assertEqual(pq.pop(), "Task 1")
     self.assertIsNone(pq.pop())
 
